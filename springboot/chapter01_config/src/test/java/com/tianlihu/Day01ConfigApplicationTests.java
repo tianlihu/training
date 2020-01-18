a21271481f77d904c8014f0e6655dafe57e75c64
tianlihu <tianlihu@qq.com>
2020-01-18 13:35:49 +0800

    第一章 04 默认配置文件的路径修改
    第一章 04 默认配置文件的路径修改
    
    Co-Authored-By: tianlihu <tianlihu@qq.com>

---
+++ springboot/chapter01_config/src/test/java/com/tianlihu/Day01ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianlihu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
